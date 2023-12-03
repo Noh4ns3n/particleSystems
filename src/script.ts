22d0e6d7a9ba534467c565669c7335d6ef421e49
Noh4ns3n <rigolot.vincent@gmail.com>
2023-12-03 02:23:54 +0100

    bounce around HTML element + leave trail

--- src/script.ts
+++ src/script.ts
@@ -7,7 +7,12 @@ const effect = new Effect(canvas);
 console.log("effect :>> ", effect);
 
 function animate() {
-  effect.context.clearRect(0, 0, canvas.width, canvas.height);
+  // instead of clearRect, drawing a transparent canvas over the old one leaves trails
+  // effect.context.clearRect(0, 0, canvas.width, canvas.height);
+  effect.context.save();
+  effect.context.fillStyle='rgba(0,0,0,0.05)'
+  effect.context.fillRect(0,0,canvas.width, canvas.height);
+  effect.context.restore();
   effect.handleParticles();
   requestAnimationFrame(animate);
 }
